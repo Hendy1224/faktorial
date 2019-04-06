92a2b73a162959dbddd55fa067b86401b3efb9ca
Hendy1224 <31037833+Hendy1224@users.noreply.github.com>
2019-04-06 11:51:42 +0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// write your code here
         for (int i=1;i<=4;i++){
             for(int j=4;j>=i;j--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int k=1;k<=i;k++){
                 System.out.print("*");
@@ -14,17 +14,23 @@ public class Main {
             for (int l=1;l<=i-1;l++){
                 System.out.print("*");
             }
+            for(int x=4+1;x>=i;x--){
+                System.out.print("0");
+            }
             System.out.println();
         }
         for (int i=1;i<=5;i++){
             for(int j=1;j<=i-1;j++){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int z=5;z>=i;z--){
                 System.out.print("*");
             }
             for(int k=4;k>=i;k--){
                 System.out.print("*");
+            }
+            for(int x=1;x<=i;x++){
+                System.out.print("0");
             }
             System.out.println("");
         }
